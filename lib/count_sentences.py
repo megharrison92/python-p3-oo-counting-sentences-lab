b24592ca71b76f0df1098be060ff727ffae0d18b
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-07-11 14:59:46 -0500

    completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
+import ipdb 
+import re
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+
+  def get_value(self):
+    return self._value
+
+  def set_value(self, new_value):
+    if type(new_value) is str:
+      self._value = new_value
+    else:
+      print("The value must be a string.")
+  
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    if self.value[-1] == ".":
+      return True
+    else:
+      return False
+
+  def is_question(self):
+    if self.value[-1] == "?":
+      return True
+    else:
+      return False
+
+  def is_exclamation(self):
+    if self.value[-1] == "!":
+      return True
+    else: 
+      return False
+
+  def count_sentences(self):
+    no_questions = self.value.replace("?", ".")
+    no_exclamations = no_questions.replace("!", ".")
+    sentence_list = no_exclamations.split(".")
+    sentence_set = list(set(sentence_list))
+    sentence_set.remove("")
+    return len(sentence_set)
+ 
